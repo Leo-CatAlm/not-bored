185e373a7113b6547ba5c15fef9709d558fce37e
lcatejo <leonardo.catejo@mercadolibre.cl>
2022-09-06 11:39:04 -0400

    Add companion object to RetrofitClient

--- app/src/main/java/com/example/notbored/client/RetroFitClient.kt
+++ app/src/main/java/com/example/notbored/client/RetroFitClient.kt
@@ -1,4 +1,20 @@
 package com.example.notbored.client
 
+import com.example.notbored.service.ActivitiesService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class RetroFitClient {
+
+    companion object{
+        private const val base_url = "https://www.boredapi.com"
+
+        fun getInstance(url: String) : ActivitiesService {
+            val retrofit = Retrofit.Builder()
+                .baseUrl(base_url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            return retrofit.create(ActivitiesService::class.java)
+        }
+    }
 }

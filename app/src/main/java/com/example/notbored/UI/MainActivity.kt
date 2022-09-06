36fed22f8b88afa3705e2e6662784b2fa7e03649
sdepetris <stefano.depetris@mercadolibre.com>
2022-09-06 09:22:32 -0300

    Agregado de binding y estructura de main

--- app/src/main/java/com/example/notbored/UI/MainActivity.kt
+++ app/src/main/java/com/example/notbored/UI/MainActivity.kt
@@ -1,12 +1,49 @@
 package com.example.notbored.UI
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.whenCreated
 import com.example.notbored.R
+import com.example.notbored.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setListeners()
+
+        setContentView(view)
+    }
+
+    fun setListeners(){
+        binding.btnStart.setOnClickListener {
+
+            val numberParticipants=binding.etNumber.text
+            when{
+                numberParticipants.isNullOrEmpty()-> Toast.makeText(baseContext,R.string.emptyParticipans,Toast.LENGTH_LONG).show()
+                !isInteger(numberParticipants.toString())-> Toast.makeText(this,R.string.typeParticipans,Toast.LENGTH_LONG).show()
+                else-> Toast.makeText(this,R.string.loading,Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+        binding.tvTerm.setOnClickListener {
+            //Ir a la otra actividad
+        }
+
+    }
+    fun isInteger(s: String): Boolean {
+        return try {
+            s.toInt()
+            true
+        } catch (e: NumberFormatException) {
+            false
+        }
     }
 }

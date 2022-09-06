43a3087a1e62f97161318724183215670b7ba3a0
lcatejo <leonardo.catejo@mercadolibre.cl>
2022-09-06 15:15:46 -0400

    Rollback Call(retrofit2) to activitiesService due to redundance

--- app/src/main/java/com/example/notbored/service/ActivitiesService.kt
+++ app/src/main/java/com/example/notbored/service/ActivitiesService.kt
@@ -1,16 +1,14 @@
 package com.example.notbored.service
 
 import com.example.notbored.model.ActivitiesResponse
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface ActivitiesService {
 
-    //TODO: add Call<T> in both gets
     @GET("/api/activity?participants={participants}")
-    suspend fun getRandomActivity(@Path("participants") participants: Int): Call<ActivitiesResponse>
+    suspend fun getRandomActivity(@Path("participants") participants: Int): ActivitiesResponse
 
     @GET("/api/activity?type={activity}&participants={participants}")
-    suspend fun getActivity(@Path("activity") activity: String, @Path("participants") participants: Int): Call<ActivitiesResponse>
+    suspend fun getActivity(@Path("activity") activity: String, @Path("participants") participants: Int): ActivitiesResponse
 }

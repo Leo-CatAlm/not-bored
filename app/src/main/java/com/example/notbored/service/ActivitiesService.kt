72db72afa14eb4aff6ff199af6765b827d3866b8
lcatejo <leonardo.catejo@mercadolibre.cl>
2022-09-06 11:32:20 -0400

    Add get structure to service interface

--- app/src/main/java/com/example/notbored/service/ActivitiesService.kt
+++ app/src/main/java/com/example/notbored/service/ActivitiesService.kt
@@ -1,4 +1,14 @@
 package com.example.notbored.service
 
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface ActivitiesService {
+
+    //TODO: add Call<T> in both gets
+    @GET("/api/activity?participants={participants}")
+    fun getRandomActivity(@Path("participants") participants: Int)
+
+    @GET("api/activity?type={activity}&participants={participants}")
+    fun getActivity(@Path("activity") activity: String, @Path("participants") participants: Int)
 }

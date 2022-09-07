a5cd48963f7d52458a17bcfc08cccea46ebfc60a
sdepetris <stefano.depetris@mercadolibre.com>
2022-09-07 12:55:49 -0300

    bug fixed

--- app/src/main/java/com/example/notbored/service/ActivitiesService.kt
+++ app/src/main/java/com/example/notbored/service/ActivitiesService.kt
@@ -4,6 +4,7 @@ import com.example.notbored.model.ActivitiesResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ActivitiesService {
 

411258f0234c94fd715f8ad20dacfe0f4819e9b8
lcatejo <leonardo.catejo@mercadolibre.cl>
2022-09-07 12:45:32 -0400

    Get the response in the final activity, and bind views with it

--- app/src/main/java/com/example/notbored/ui/ActivityDetail.kt
+++ app/src/main/java/com/example/notbored/ui/ActivityDetail.kt
@@ -3,6 +3,7 @@ package com.example.notbored.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.notbored.databinding.ActivityDetailBinding
+import com.example.notbored.model.ActivitiesResponse
 
 class ActivityDetail: AppCompatActivity()  {
 
@@ -11,5 +12,20 @@ class ActivityDetail: AppCompatActivity()  {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val response = intent.getSerializableExtra("activityType") as ActivitiesResponse
+
+        with(binding){
+            tvActivityType.text = response.type
+            tvActivityTitle.text = response.activity
+            tvNumberOfParticipants.text = response.participants.toString()
+
+            when(response.price){
+                0.0 -> tvPrice.text = "Free"
+                /*0 .. 0.3 -> tvPrice.text = "Low"
+                0 .. 0.3 -> tvPrice.text = "Medium"
+                0 .. 0.3 -> tvPrice.text = "High"*/
+            }
+        }
     }
 }

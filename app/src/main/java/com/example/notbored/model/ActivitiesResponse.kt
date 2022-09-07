b6bb3f9749f7b97354ea120e241c5a7f878b5f95
lcatejo <leonardo.catejo@mercadolibre.cl>
2022-09-07 12:25:44 -0400

    Make dataclass serializable and add putextra in the respective instance

--- app/src/main/java/com/example/notbored/model/ActivitiesResponse.kt
+++ app/src/main/java/com/example/notbored/model/ActivitiesResponse.kt
@@ -1,8 +1,10 @@
 package com.example.notbored.model
 
+import java.io.Serializable
+
 data class ActivitiesResponse (
         val activity: String,
         val type: String,
         val participants: Int,
         val price: Double
-        )
+        ): Serializable
